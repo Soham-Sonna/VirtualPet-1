7bd45b2d6f0791b2f11011b097855643a4ef6ef3
Soham-Sonna <66825123+Soham-Sonna@users.noreply.github.com>
2020-07-17 21:02:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var dog, happyDog, database, foodS, foodStock
+
+function preload()
+{
+    dogImg = loadImage("images/dogImg.png");
+
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
